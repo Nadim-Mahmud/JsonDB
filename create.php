079dcc0db01150a18aa63895d68fa28a84476512
Nadim-Mahmud <mahmudnadim16@gmail.com>
2022-01-06 10:11:03 +0600

    ui modifiactions

--- create.php
+++ create.php
@@ -13,7 +13,7 @@ if (isset($_GET['title'])) {
     $data = array(
         'title' => htmlspecialchars($_GET['title']),
         'author' => $_GET['author'],
-        'available' => $_GET['available'] === "true",
+        'available' => $_GET['available'],
         'pages' => $_GET['pages'],
         'isbn' => $_GET['isbn']
     );
@@ -36,49 +36,62 @@ if (isset($_GET['title'])) {
     <meta charset="UTF-8">
     <title>Add New</title>
     <link rel="stylesheet" type="text/css" href="style.css" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
 
 <body class="create-body">
 
     <h1 class="title"> Enter book informations</h1>
 
+    <!-- Styled -->
+
+    <!-- create new item -->
+
     <div class="form-container">
-        <form class="" action="" method="get">
-            <div>
-                <label class="form-head">Title</label>
-                <br>
-                <input type="text" name="title" />
+        <form action="" method="GET">
+
+            <div class="form-left form-group">
+                <label>Title</label>
+                <input type="text" class="form-control" placeholder="Title" name="title" required>
             </div>
-            <div>
-                <label class="form-head">Author</label>
-                <br>
-                <input type="text" name="author" />
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for=>Author</label>
+                    <input type="text" class="form-control" placeholder="Author" name="author" required>
+                </div>
+                <div class="form-group col-md-6">
+                    <label>Avilability</label>
+                    <select class="form-control" name="available" required>
+                        <option value="true" Selected>True</option>
+                        <option value="false">False</option>
+                    </select>
+                </div>
             </div>
-            <div>
-                <label class="form-head">Available</label>
-                <br>
-                <input type="text" name="available" />
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label>Pages</label>
+                    <input type="number" class="form-control" placeholder="Pages" name="pages" required>
+                </div>
+                <div class="form-group col-md-6">
+                    <label>ISBN</label>
+                    <input type="number" class="form-control" placeholder="ISBN" name="isbn" required>
+                </div>
             </div>
-            <div>
-                <label class="form-head">Pages</label>
-                <br>
-                <input type="text" name="pages" />
-            </div>
-            <div>
-                <label class="form-head">ISBN</label>
-                <br>
-                <input type="text" name="isbn" />
-            </div>
-            <input class="btn btn-success create-btns" type="submit" value="Create" />
+
+
+            <button type="submit" class="form-sub btn btn-success">Create</button>
         </form>
 
     </div>
 
-    <!-- bootstrap scripts -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
+    <!-- bootstrap scripts -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
+    </script>
 
 </body>
 
