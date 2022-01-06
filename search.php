72c65812326b6f8a364f587e72955299ac457d7a
Nadim-Mahmud <mahmudnadim16@gmail.com>
2022-01-06 23:13:52 +0600

    header fixed

--- search.php
+++ search.php
@@ -38,7 +38,7 @@ if (isset($_POST['search'])) {
 
 <head>
     <meta charset="UTF-8">
-    <title>Book Store</title>
+    <title>Search</title>
     <link rel="stylesheet" type="text/css" href="style.css" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">    
 </head>
